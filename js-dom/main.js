d184dd67628b922967a2de478118f32c57facdd6
cavansir112 <44567209+cavansir112@users.noreply.github.com>
2024-07-26 16:06:26 +0400

    add arry 4

---
+++ js-dom/main.js
@@ -0,0 +1,43 @@
+
+
+// task 1
+// function Ok(a){
+//     let inp1 = +document.getElementById("inp1").value;
+//     let inp2 = +document.getElementById("inp2").value;
+//     if(a.innerText == "+"){
+//         alert(inp1 + inp2);
+//     }
+//     else if(a.innerText == "-"){
+//         alert(inp1 - inp2);
+//     }
+//     else if(a.innerText == "*"){
+//         alert(inp1 * inp2);
+//     }
+//     if(a.innerText == "/"){
+//         alert(inp1 / inp2);
+//     }
+// }
+
+
+
+
+// task 2
+// let btn =document.getElementById("btn");
+
+// btn.onclick = function () {
+//     let one = inputR.value;
+//     inputR.value = ""
+//     let yer = "";
+
+//     for(let i = one.length-1; i >= 0; i--){
+//         yer += one[i];
+
+//     }
+//     alert(yer);
+// }
+
+
+
+
+//  task 3
+
